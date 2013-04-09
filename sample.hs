77c424f6169e40ed10a58ad6e2777263dc3bfc6e
Diwakar Chauhan <diwakar.chauhan231@gmail.com>
2013-04-09 23:15:02 +0530

    modified:   sample.hs

--- sample.hs
+++ sample.hs
@@ -8,5 +8,7 @@ import Language.Haskell.Extension
 
 --create array by y =  fromListUnboxed(Z :.(4::Int):.(4::Int)) [1..16]
 createArr::(Array U ((Z :. Int) :. Int) Double ) -> Int ->Int ->(IO (Array U DIM2 Double))
-createArr  y row col= computeP $ traverse y id (\f (Z:.i :. j) -> if j  > 0 && i  > 0 && j < col -1 && i < row -1 then(f(Z:.i :.j) + f(Z:.i :.j+1)+f(Z:.i :.j-1) + f(Z:.i+1 :.j))/5
+createArr  y row col= computeP $ traverse y id (\f (Z:.i :. j) -> if j  > 0 && i  > 0 && j < col -1 && i < row -1 then(sum'' (f(Z:.i :.j))  (f(Z:.i :.j+1)) (f(Z:.i :.j-1))  (f(Z:.i+1 :.j)) (f(Z:.i-1 :.j)))
                                                                   else f(Z:.i:.j))
+                                                                  
+sum'' x1 x2 x3 x4 x5 = (x1+x2+x3+x4+x5)/5                                                                  
